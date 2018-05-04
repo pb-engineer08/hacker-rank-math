e576f84d750e1630c14d85646f5f74d1c84d95e7
projesh <projesh>
2018-05-04 20:10:09 +0600

    point reflection

---
+++ hacker-rank-math/test.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+# from __future__ import print_function
+
+import os
+import sys
+
+#
+# Complete the findPoint function below.
+#
+# Midpoint of two point 
+def findPoint(px, py, qx, qy):
+    l=list()
+    nX=2*qx-px
+    nY=2*qy-py
+    l.append(nX)
+    l.append(nY)
+    # print(nX)
+    return l
+
+
+n = int(raw_input())
+
+for n_itr in xrange(n):
+    pxPyQxQy = raw_input().split()
+
+    px = int(pxPyQxQy[0])
+
+    py = int(pxPyQxQy[1])
+
+    qx = int(pxPyQxQy[2])
+
+    qy = int(pxPyQxQy[3])
+
+    result = findPoint(px, py, qx, qy)
+    print(' '.join(map(str,result)))
